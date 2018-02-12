1db432a0c3ef88eeecc7b2c04c8b398308b9b803
harshdeep gupta <hdgiitd@gmail.com>
2018-02-13 00:25:06 +0530

    get backprop working on XOR dataset

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def softmax(x, deriv = False):
         return Null
     else:
         expos = np.exp(x - np.max(x))
-        return expos/np.sum(expos)
+        return expos/np.sum(expos, axis = 1, keepdims=True)
 
 def activation(x, type = 'sigmoid', deriv = False):
     if type == 'sigmoid':
@@ -45,52 +45,79 @@ def activation(x, type = 'sigmoid', deriv = False):
 def cross_entropy_loss(y_pred, y_target, deriv = False):
     '''y_target is a one hot encoded vector, y_pred is the output from softmax layer'''
     if deriv:
-        # TODO
         return y_pred - y_target
     else:
-        return -np.dot(y_target, np.log10(y_pred).T)
+        return -np.dot(y_target.T, np.log(y_pred))
 
-def train(x,t,w_1,w_2,b_1,b_2,alpha = 0.1, learning_rate = 0.01):
-    '''x is the input data,t is the target, v and w are layers, bv and bw are the biases'''
+def forward_pass(x,w_1,w_2,b_1,b_2):
     # forward propagation : matrix multiplication + biases
-    a_1 = np.dot(x,w_1) + b_1
-    z_1 = activation(a_1)
+    # print("x1 shape = {}".format(x.shape))
+    # print("w1 shape = {}".format(w_1.shape))
+    a_2 = np.dot(x,w_1) + b_1
+    # print("a2 shape = {}".format(a_2.shape))
+    z_2 = activation(a_2)
+    # print("z2 shape = {}".format(z_2.shape))
 
-    a_2 = np.dot(z_1, w_2) + b_2
-    # TODO: implement softmax
-    z_2 = activation(a_2, 'softmax')
-    y_pred = z_2
+    a_3 = np.dot(z_2, w_2) + b_2
+    # print("a3 shape = {}".format(a_3.shape))
+    z_3 = activation(a_3, 'softmax')
+    y_pred = z_3
+    return y_pred
 
-    # calculate loss
-    loss = cross_entropy_loss(y_pred,t)
-    # calculate gradient of loss
-    delta_3 = cross_entropy_loss(y_pred,t,deriv = True)
-    # Backward propagation : calculate the errors and gradients
-    dw_2 = np.dot(a_1.T,delta_3)
-    db_2 = np.sum(delta_3, axis=0, keepdims=True)
-    delta_2 = np.dot(delta_3,w_2.T)* activation(a_1,deriv = True)
-    dw_1 = np.dot(x.T, delta_2)
-    db_1 = np.sum(delta_2, axis=0,keepdims=True)
 
-    # Add regularization terms (b1 and b2 don't have regularization terms)
-    dw_2 += alpha * w_2
-    dw_1 += alpha * w_1
 
-    # Do the gradient descent parameter update
-    w_2 += -learning_rate*dw_2
-    w_1 += -learning_rate*dw_1
-    b_1 += -learning_rate*db_1
-    b_2 += -learning_rate*db_2
+def train(x,t,w_1,w_2,b_1,b_2,alpha = 0, learning_rate = 0.01):
+    '''x is the input data,t is the target, v and w are layers, bv and bw are the biases'''
+    for _ in range(1000000):
+        # forward propagation : matrix multiplication + biases
+        # print("x1 shape = {}".format(x.shape))
+        # print("w1 shape = {}".format(w_1.shape))
+        a_2 = np.dot(x,w_1) + b_1
+        # print("a2 shape = {}".format(a_2.shape))
+        z_2 = activation(a_2)
+        # print("z2 shape = {}".format(z_2.shape))
 
+        a_3 = np.dot(z_2, w_2) + b_2
+        # print("a3 shape = {}".format(a_3.shape))
+        z_3 = activation(a_3, 'softmax')
+        y_pred = z_3
+        # print("z3 shape = {}".format(z_3.shape))
+
+        # calculate loss
+        loss = cross_entropy_loss(y_pred,t)
+        # print(loss)
+        # calculate gradient of loss    
+        delta_3 = cross_entropy_loss(y_pred,t,deriv = True)
+        # print("delta3  = {}".format(delta_3))
+        # Backward propagation : calculate the errors and gradients
+        dw_2 = np.dot(a_2.T,delta_3)
+        db_2 = np.sum(delta_3, axis=0, keepdims=True)
+        delta_2 = np.dot(delta_3,w_2.T)* activation(z_2,deriv = True)
+        # print("delta_2 shape = {}".format(delta_2.shape))
+        dw_1 = np.dot(x.T, delta_2)
+        db_1 = np.sum(delta_2, axis=0,keepdims=True)
+
+        # Add regularization terms (b1 and b2 don't have regularization terms)
+        dw_2 += alpha * w_2
+        dw_1 += alpha * w_1
+
+        # Do the gradient descent parameter update
+        # print("w2 update = {}".format(-learning_rate*dw_2))
+        w_2 += -learning_rate*dw_2
+        w_1 += -learning_rate*dw_1
+        b_1 += -learning_rate*db_1
+        b_2 += -learning_rate*db_2
+
+    return w_2,b_2,w_1,b_1
 
 
 
 
 # network topology
-INPUT_SIZE = 784
-H1_SIZE = 500
-OUTPUT_Size = 6
-BATCH_SIZE = 32
+INPUT_SIZE = 2
+H1_SIZE = 6
+OUTPUT_Size = 2
+BATCH_SIZE = 4
 # hyperparameter
 learning_rate = 0.01
 alpha = 0.1
@@ -104,13 +131,17 @@ def main():
     # W = np.array([[1,2,3],[2,4,5],[2,3,4],[3,4,5]])
     # print(x.shape)
     # print(sigmoid(np.dot(x,W)))
-    x = np.random.rand(BATCH_SIZE,INPUT_SIZE)
+    # x = np.random.rand(BATCH_SIZE,INPUT_SIZE)
+    x = np.array([[1,0],[0,1],[0,0],[1,1]])
+    # target = np.random.rand(BATCH_SIZE,OUTPUT_Size)
+    target = np.array([[1,0],[1,0],[0,1],[0,1]])
     w_1 = np.random.rand(INPUT_SIZE, H1_SIZE)
     w_2 = np.random.rand(H1_SIZE, OUTPUT_Size)
-    b_1 = np.random.rand(1, H1_SIZE)
-    b_2 = np.random.rand(1, OUTPUT_Size)
-    target = np.random.rand(BATCH_SIZE,OUTPUT_Size)
-    train(x,target,w_1,w_2,b_1,b_2)
-
+    b_1 = np.ones([1, H1_SIZE])
+    b_2 = np.ones([1, OUTPUT_Size])
+    
+    print(forward_pass(x,w_1,w_2,b_1,b_2))
+    w_2, b_2, w_1, b_1 = train(x,target,w_1,w_2,b_1,b_2)
+    print(forward_pass(x,w_1,w_2,b_1,b_2))
 if __name__ == "__main__":
     main()
