97547a9f9397a65e57e0b43381c15f3be1520285
harshdeep gupta <hdgiitd@gmail.com>
2018-02-13 03:52:31 +0530

    Test a deep network

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from Layer import Layer
 
 def main():
     model = Sequential()
-    model.add(Layer(size = 2))
-    model.add(Layer(size = 6,activation = 'sigmoid'))
-    model.add(Layer(size = 2,activation = 'softmax'))
-    model.compile()
+    model.add(Layer(size = 2)) # Input layer
+    model.add(Layer(size = 6,activation = 'sigmoid')) # Hidden layer with 6 neurons
+    model.add(Layer(size = 6,activation = 'sigmoid')) # Hidden layer with 6 neurons
+    model.add(Layer(size = 2,activation = 'softmax')) # Output layer
+    model.compile(learning_rate = 0.1)
     # learn the XOR mapping
     X =  np.array([[1,0],[0,1],[0,0],[1,1]])
     Y =  np.array([[1,0],[1,0],[0,1],[0,1]])
