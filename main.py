c2be3e9c9e2938f80faca33823ea422c1e7f4aa7
harshdeep gupta <hdgiitd@gmail.com>
2018-02-14 05:16:16 +0530

    test relu network

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from Layer import Layer
 def main():
     model = Sequential()
     model.add(Layer(size = 2)) # Input layer
-    model.add(Layer(size = 6,activation = 'sigmoid')) # Hidden layer with 6 neurons
-    model.add(Layer(size = 6,activation = 'sigmoid')) # Hidden layer with 6 neurons
+    model.add(Layer(size = 6,activation = 'relu')) # Hidden layer with 6 neurons
+    model.add(Layer(size = 6,activation = 'relu')) # Hidden layer with 6 neurons
     model.add(Layer(size = 2,activation = 'softmax')) # Output layer
     model.compile(learning_rate = 0.1)
     # learn the XOR mapping
